dcaab78f9cb5ce50b5ee16d9ece63ee98491d580
dev@powerdigital.id <raflyputra0510@gmail.com>
2023-07-04 00:46:15 +0700

    fix upload file + delete

--- components/siswa/Laporan.jsx
+++ components/siswa/Laporan.jsx
@@ -1,13 +1,38 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Swal from "sweetalert2";
 import { supabase } from "../../libs/supabase.lib";
 import Card from "../utils/card";
+import useLoginStore from "../../store/store";
 
 export default function Upload() {
+  const user = useLoginStore((state) => state.user);
   const [isUploading, setIsUploading] = useState(false);
   const [uploadMessage, setUploadMessage] = useState("Upload");
   const [fileName, setFileName] = useState("");
-  const [kegiatan, setKegiatan] = useState("");
+  const [dataSiswa, setDataSiswa] = useState([]);
+  const [laporan, setLaporan] = useState([]);
+  // const [kegiatan, setKegiatan] = useState("");
+
+  const handleGetUserData = async () => {
+    let { data, error } = await supabase.from('User').select().eq('username', user)
+    if (error) {
+      console.error(error)
+    } else {
+      console.log(data);
+      handleGetSiswaData(data[0].id)
+    }
+  }
+
+  const handleGetSiswaData = async (userid) => {
+    let { data, error } = await supabase.from('DataSiswa').select().eq('userId', userid)
+    if (error) {
+      console.error(error)
+    } else {
+      console.log(data)
+      setDataSiswa(data)
+    }
+  }
+
   const uploadFile = async (event) => {
     try {
       setIsUploading(true);
@@ -32,42 +57,94 @@ export default function Upload() {
       setIsUploading(false);
     }
   };
-  const handleCreateLog = async (e) => {
+
+  const handleUploadFile = async (e) => {
     try {
       e.preventDefault();
-      if (fileName == "")
-        throw new Error("Anda harus memilih satu gambar untuk diunggah!");
-      const newLog = {
-        nama: "guru",
-        kegiatan: kegiatan,
+      if (fileName == '') throw new Error('Anda harus memilih satu gambar untuk diunggah!');
+      const newFile = {
+        nama: dataSiswa[0].nama_siswa,
+        SiswaId: dataSiswa[0].nisn,
         createdAt: new Date(Date.now()).toISOString(),
-        image: fileName,
-      };
-      let { data, error } = await supabase
-        .from("LogGuru")
-        .insert(newLog)
-        .select();
+        file: fileName
+      }
+      let { data, error } = await supabase.from('Laporan').insert(newFile).select()
       if (error) {
-        console.error(error);
+        console.error(error)
       } else {
-        Swal.fire("Success", "Log berhasil ditambahkan!", "info");
-        setFileName("");
-        setUploadMessage("Upload");
-        e.target.files = [];
-        console.info(data);
+        Swal.fire('Success', 'File berhasil ditambahkan!', 'info')
+        setFileName('')
+        setUploadMessage('Upload')
+        e.target.files = []
+        console.info(data)
       }
     } catch (error) {
-      Swal.fire(
-        "Error",
-        "Anda harus memilih satu gambar untuk diunggah!",
-        "error"
-      );
+      Swal.fire('Error', 'Anda harus memilih satu gambar untuk diunggah!', 'error')
     }
-  };
+  }
+
+  const fetchLaporan = async () => {
+    let { data, error } = await supabase.from('Laporan').select('*')
+    if (error) {
+      console.error(error)
+    } else {
+      setLaporan(data)
+    }
+  }
+
+  const handleDeleteFile = async (id) => {
+    let { error } = await supabase.from('Laporan').delete().eq('id', id)
+    if (error) {
+      console.error(error)
+      Swal.fire('Error', 'Gagal Menghapus', 'error')
+    } else {
+      Swal.fire('Success', 'Data Terhapus', 'success')
+    }
+  }
+
+  useEffect(() => {
+    handleGetUserData()
+  }, [])
+
+  useEffect(() => {
+    fetchLaporan()
+  }, [laporan])
+  // const handleCreateLog = async (e) => {
+  //   try {
+  //     e.preventDefault();
+  //     if (fileName == "")
+  //       throw new Error("Anda harus memilih satu gambar untuk diunggah!");
+  //     const newLog = {
+  //       nama: "guru",
+  //       kegiatan: kegiatan,
+  //       createdAt: new Date(Date.now()).toISOString(),
+  //       image: fileName,
+  //     };
+  //     let { data, error } = await supabase
+  //       .from("LogGuru")
+  //       .insert(newLog)
+  //       .select();
+  //     if (error) {
+  //       console.error(error);
+  //     } else {
+  //       Swal.fire("Success", "Log berhasil ditambahkan!", "info");
+  //       setFileName("");
+  //       setUploadMessage("Upload");
+  //       e.target.files = [];
+  //       console.info(data);
+  //     }
+  //   } catch (error) {
+  //     Swal.fire(
+  //       "Error",
+  //       "Anda harus memilih satu gambar untuk diunggah!",
+  //       "error"
+  //     );
+  //   }
+  // };
   return (
     <div className="container-fluid">
       <Card cardTitle="Upload Laporan" cardIcon="fa-upload">
-        <form onSubmit={handleCreateLog}>
+        <form onSubmit={handleUploadFile}>
           <div className="form-group">
             <label htmlFor="exampleInputFile">Pilih File</label>
             <div className="input-group">
@@ -97,35 +174,42 @@ export default function Upload() {
         </form>
       </Card>
       <div className="row">
-          <div className="col-12">
-            <div className="card">
-              <div className="card-header">
-                <h3 className="card-title">Tabel File Laporan</h3>
-              </div>
-              {/* /.card-header */}
-              <div className="card-body table-responsive p-0">
-                <table className="table table-hover text-nowrap">
-                  <thead className="">
-                    <tr>
-                      <th style={{width: "20px"}}>No</th>
-                      <th>Nama File</th>
-                      <th>Action</th>
-                    </tr>
-                  </thead>
-                  <tbody>
-                      <tr >
-                        <td>1</td>
-                        <td>tes.pdf <a href="#"> [ download ]</a></td>
-                        <td><a type="button" className="btn btn-danger"><i className="fas fa-trash" /></a></td>
-                      </tr>
-                  </tbody>
-                </table>
-              </div>
-              {/* /.card-body */}
+        <div className="col-12">
+          <div className="card">
+            <div className="card-header">
+              <h3 className="card-title">Tabel File Laporan</h3>
             </div>
-            {/* /.card */}
+            {/* /.card-header */}
+            <div className="card-body table-responsive p-0">
+              <table className="table table-hover text-nowrap">
+                <thead className="">
+                  <tr>
+                    <th style={{ width: "20px" }}>No</th>
+                    <th>Nama Siswa</th>
+                    <th>Nama File</th>
+                    <th>Action</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {
+                    laporan.map((siswa, i) => (
+                      <tr key={i}>
+                        <td>{i + 1}</td>
+                        <td>{siswa.nama}</td>
+                        <td>{siswa.file}<a href={`https://xuiabtqtmfhgdqyrppex.supabase.co/storage/v1/object/public/file-laporan/${siswa.file}`}> [ download ]</a></td>
+                        <td><a type="button" onClick={() => handleDeleteFile(siswa.id)} className="btn btn-danger"><i className="fas fa-trash" /></a></td>
+                      </tr>
+                    ))
+                  }
+
+                </tbody>
+              </table>
+            </div>
+            {/* /.card-body */}
           </div>
+          {/* /.card */}
         </div>
+      </div>
     </div>
   );
 }
